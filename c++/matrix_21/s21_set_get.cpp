92e57a5c8b0f256fbd9dcb7f3c17bba90b38d176
shifter <vanfre93@gmail.com>
2023-03-14 11:25:29 +0300

    add extension

--- c++/matrix_21/s21_set_get.cpp
+++
@@ -1,9 +0,0 @@
-#include "s21_matrix_oop.h"
-
-void S21Matrix::setM() { matrix = nullptr; }
-void S21Matrix::setR(int val) { _rows = val; }
-void S21Matrix::setC(int val) { _cols = val; }
-
-int S21Matrix::getR() const { return _rows; }
-int S21Matrix::getC() const { return _cols; }
-double **S21Matrix::getM() const { return matrix; }
